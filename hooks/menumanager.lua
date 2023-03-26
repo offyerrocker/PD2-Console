afdd06b374b6980be332147f7c9103a26f9121e5
offyerrocker <offyerrocker88@gmail.com>
2023-03-26 07:58:22 -0700

    Fixed chat window duplicating focus
    Fixed chat window detecting open-chat keybind (default "T") during console input focus (happens in online lobbies specifically), resulting in subsequent inputs duplicating across both console window and chat window

--- hooks/menumanager.lua
+++ hooks/menumanager.lua
@@ -1,3 +1,11 @@
 if not BeardLib then
 	dofile(ModPath .. "Core.lua")
 end
+
+local orig_toggle_chatinput = MenuManager.toggle_chatinput
+function MenuManager:toggle_chatinput(...)
+	if Console._window_instance and Console._window_instance:is_focused() then
+		return
+	end
+	return orig_toggle_chatinput(self,...)
+end
